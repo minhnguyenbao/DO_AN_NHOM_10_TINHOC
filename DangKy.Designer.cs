0cb5e7d0d58dd3f5669d971bf67ed77e19cbdaa6
ManhDoo <dodanhmanha21819@gmail.com>
2023-11-04 10:06:13 +0700

    Change git ignore

---
+++ DangKy.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class DangKy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_NhapLaiMK = new System.Windows.Forms.TextBox();
+            this.txt_MK = new System.Windows.Forms.TextBox();
+            this.txt_TenTK = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(340, 312);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 45);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Đăng ký";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txt_NhapLaiMK
+            // 
+            this.txt_NhapLaiMK.Location = new System.Drawing.Point(379, 206);
+            this.txt_NhapLaiMK.Name = "txt_NhapLaiMK";
+            this.txt_NhapLaiMK.Size = new System.Drawing.Size(279, 26);
+            this.txt_NhapLaiMK.TabIndex = 6;
+            // 
+            // txt_MK
+            // 
+            this.txt_MK.Location = new System.Drawing.Point(379, 148);
+            this.txt_MK.Name = "txt_MK";
+            this.txt_MK.Size = new System.Drawing.Size(279, 26);
+            this.txt_MK.TabIndex = 7;
+            // 
+            // txt_TenTK
+            // 
+            this.txt_TenTK.Location = new System.Drawing.Point(379, 94);
+            this.txt_TenTK.Name = "txt_TenTK";
+            this.txt_TenTK.Size = new System.Drawing.Size(279, 26);
+            this.txt_TenTK.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(142, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nhập lại mật khẩu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(142, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Mật khẩu";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(142, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Tên tài khoản";
+            // 
+            // DangKy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_NhapLaiMK);
+            this.Controls.Add(this.txt_MK);
+            this.Controls.Add(this.txt_TenTK);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "DangKy";
+            this.Text = "DangKy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_NhapLaiMK;
+        private System.Windows.Forms.TextBox txt_MK;
+        private System.Windows.Forms.TextBox txt_TenTK;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,40 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        string tenTK = "admin";
+        string MK = "123456";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void dangnhap_Click(object sender, EventArgs e)
+        {
+            if (tenTK == null || tenTK.Equals(""))
+                MessageBox.Show("Hãy nhập tên tài khoản");
+            if (MK == null || MK.Equals(""))
+                MessageBox.Show("Hãy nhập mật khẩu");
+            if (kiemTraDangNhap(txtTK.Text, txtMK.Text))
+            {
+                MessageBox.Show("Đăng nhập thành công");
+                QLBaiDoXe sh = new QLBaiDoXe();
+                sh.ShowDialog();
+                sh.Hide();
+            }
+            else
+                MessageBox.Show("Tên tài khoản hoặc mật khẩu sai!");
+        }
+        bool kiemTraDangNhap(string tk, string mk)
+        {
+            if (this.tenTK == tk && this.MK == mk)
+                return true;
+            return false;
+        }
+
+        private void dangKy_Click(object sender, EventArgs e)
+        {
+            DangKy dk = new DangKy();
+            dk.ShowDialog();
         }
     }
 }
